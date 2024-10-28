375344d4843fd0ed6f6c3de9f9c677595e641a4f
geovanna <christinageovanna75@gmail.com>
2024-10-28 14:47:31 -0300

    novo

---
+++ script.js
@@ -0,0 +1,36 @@
+//condicionais são uma forma de tomar decisões, de fazer o computador executar essas decisões
+
+//if significa "se"
+//else significa "senão"
+
+//if (condição) {código que será eecutado}
+
+//variáveis
+//3 var, let, const
+//prompt pergunta algo ao usuário
+//sempre que usamos dados textuais ""
+//string
+
+//let nome= prompt ("Qual é o seu nome?")
+
+//alert usamos para eibir uma mensagem 
+//alert ("Seja bem-vindo(a)," + nome);
+
+//let idade= prompt ("Quantos anos você tem?")
+//if significa "se"
+//else significa "senão"
+//if (condição) {código que será executado}
+// if (idade >=18) {
+//      alert ("Você já pode tirar a carteira de motorista!!!")
+
+// }else{
+//      alert("Você ainda não tem idade para dirigir!")
+// }
+
+let gostarDePizza = prompt("você gosta de pizza? responda (sim ou não) ")
+//===estritamente igual
+if(gostarDePizza === "Sim"){
+    alert("Que ótimo! Vamos comer juntos!")
+}else{
+ alert("Tudo bem, talvez você goste de outra comida");
+}
